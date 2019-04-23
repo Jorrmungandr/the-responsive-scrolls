eae6f85b1c4cc69ead6a52f98a56b69b337a14f8
Yorrmungand <edgarmarques@hotmail.com.br>
2019-04-22 22:30:42 -0300

    Made react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class ResponsiveContainer extends Component {
+  render() {
+    return(
+      <div class="responsive-container">
+        
+      </div>
+    );
+  }
+} 
+
+export default Responsive;
