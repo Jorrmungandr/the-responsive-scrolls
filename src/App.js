a61353e56d992d123a2eb6afafd2e1be57fc8a76
Yorrmungand <edgarmarques@hotmail.com.br>
2019-04-27 19:18:35 -0300

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class ResponsiveContainer extends Component {
   render() {
-    return(
+    return (
       <div class="responsive-container">
-        
+
       </div>
     );
   }
-} 
+}
 
-export default Responsive;
+class Navbar extends Component {
+  render() {
+    return (
+      <div class="navbar">
+        <div id="text-container">
+          <a>Made with &lt;/&gt; and &hearts; by Jorrmungandr</a>
+        </div>
+        <div>
+        <div class="menu"><a>Download</a></div>
+        <div class="menu"><a>Search</a></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Page extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <ResponsiveContainer />
+      </div>
+    );
+  }
+}
+
+export default Page;
