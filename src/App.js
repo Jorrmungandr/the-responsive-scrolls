b6e6217c7b6d9eebc8f6df78c3d5cc93efb2c27d
Yorrmungand <edgarmarques@hotmail.com.br>
2019-04-27 22:44:44 -0300

    Added Resizing divs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class ResponsiveContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventX: 0,
+      eventY: 0,
+      isMouseDown: false,
+    }
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      eventX: event.clientX,
+      eventY: event.clientY,
+    });
+  }
+
+  handleResize(event) {
+    if (this.state.isMouseDown) {
+      let direction = event.target.className.split('-')[0];
+      if (direction === 'right') {
+        // event.target.parentElement.parentElement.style.width = this.state.eventX + 'px';
+        console.log(event.target.style.height);
+      }
+    }
+  }
+  
+  handleMouseDown(event){
+    this.setState({
+      isMouseDown: true,
+    })
+  }
+  
+  handleMouseUp(event){
+    this.setState({
+      isMouseDown: false,
+    })
+  }
+
   render() {
     return (
-      <div id="ette">
+      <div id="ette" onMouseMove={this.handleMouseMove.bind(this)} onMouseDown={this.handleMouseDown.bind(this)} onMouseUp={this.handleMouseUp.bind(this)}>
         <div class="responsive-container">
-          <div class="responsive-component" id="responsive-navbar">
-            <a>One</a>
-            <a>Two</a>
-            <a>Three</a>
+          <div>
+            <p>Responsive Navbar:</p>
+            <div class="top-resizer"></div>
+            <div class="responsive-component" id="responsive-navbar">
+              <div class="left-resizer"></div>
+              <a>One</a>
+              <a>Two</a>
+              <a>Three</a>
+              <div class="right-resizer" onMouseMove={this.handleResize.bind(this)}></div>
+            </div>
+            <div class="bottom-resizer"></div>
           </div>
-          <div class="responsive-component"></div>
-          <div class="responsive-component"></div>
         </div>
       </div>
     );
