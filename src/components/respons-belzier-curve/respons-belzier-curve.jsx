127e98a16f16405d9ce471fb6b37a32d51555645
Edgar Angelo de Lima e Marques <ealm2@ug1c45.windows.cin.ufpe.br>
2019-07-01 11:21:26 -0300

    Finish responsive belzier curve

--- src/components/respons-belzier-curve/respons-belzier-curve.jsx
+++ src/components/respons-belzier-curve/respons-belzier-curve.jsx
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import './respons-belzier-curve';
+import './respons-belzier-curve.scss';
 
 export default class ResponsiveBelzierCurve extends Component {
   constructor(props) {
     super(props);
-  }
-
-  componentDidMount() {
-    document.querySelector('.responsive-belzier-curve').style.display = 'flex';
   }
 
   render() {
@@ -18,11 +14,9 @@ export default class ResponsiveBelzierCurve extends Component {
           <div class="top-resizer"></div>
           <div class="responsive-belzier-curve">
             <div class="left-resizer"></div>
-            <div>
-              <svg height="100%" width="100%" viewBox="0 0 100 50" style={{background: '#333',color: 'turquoise',}} class="belzier-curve-container">
-                <path d="M0,0 Q50,100 100,0 Z" id="belzier-curve" fill="turquoise"/>
-              </svg>
-            </div>
+            <svg viewBox="0 0 300 50" class="belzier-curve-container">
+              <path d="M0,0 Q150,100 300,0 Z" id="belzier-curve"/>
+            </svg>
             <div class="right-resizer belzier-resizer" onMouseDown={this.props.handleMouseDown}></div>
           </div>
           <div class="bottom-resizer"></div>
